77a0cb7da454a06dd2dea6f2884aff89dfdfa25a
Mark D. King <kingmarkd@gmail.com>
2018-03-11 17:05:14 -0400

    Phone3

--- public/js/script.js
+++ public/js/script.js
@@ -13,6 +13,16 @@ $(document).ready( function() {
 		$(this).css({'color': '#ffffff', 'background-color': '#000000'});
 	});
 
+	$('#expand2').click( function() {
+		$('#instructions').css('transform', 'translate(-450px, 0px)');
+	});
+
+	$('#expand2').hover( function() {
+		$(this).css({'color': '#111111', 'background-color': '#f1f1f1'});
+		}, function() {
+		$(this).css({'color': '#ffffff', 'background-color': '#000000'});
+	});
+
 	$('#contract').click( function() {
 		$('#instructions').css('transform', 'translate(0px, 0px)');
 	});
